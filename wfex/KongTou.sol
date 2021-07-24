7d527587b1203f970be10265425011a514b0eb6d
mryang <479189772@qq.com>
2021-07-24 19:31:37 +0800

    修改

--- wfex/KongTou.sol
+++ wfex/KongTou.sol
@@ -19,13 +19,19 @@ contract KongTou{
         owner = _owner;
     }
     
+    event sendWfecEvn(address indexed _to,uint256 indexed _value);
+    
+    event sendWfcEvn(uint256 indexed _type,address indexed _to,uint256 indexed _usdtNum);
+    
     function sendWfec(address _to,uint256 _value) public onlyOwner{
         wfec.transfer(_to,_value);
+        emit sendWfecEvn(_to,_value);
     }
     
 
-    function sendWfc(address _to,uint256 _value) public onlyOwner{
+    function sendWfc(uint256 _type,address _to,uint256 _value,uint256 _usdtNum) public onlyOwner{
         wfc.transfer(_to,_value);
+        emit sendWfcEvn(_type,_to,_usdtNum);
     }
 
     
